c88e07ed15dffd37bb543ae8a40f64c3cc084b97
PriyaBalaDharshini <priyaarasu12@gmail.com>
2024-09-16 11:27:54 +0530

    Base URL is changed

--- frontend/src/requestMethods.js
+++ frontend/src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const BASE_URL = "http://localhost:8000"
+const BASE_URL = "https://mern-blooddonationapp.onrender.com"
 
 export const publicRequest = axios.create({
     baseURL: BASE_URL
